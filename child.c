ae088a995b1500a12254afb34cc12b96045a5abc
soufiane <soufian9mkns@gmail.com>
2023-09-16 14:09:33 -0700

    Challenger

--- child.c
+++ child.c
@@ -1,23 +1,3 @@
-#include "shell.h"
-int main(void) {
-	char *ash = malloc(1024), **argv;
-	
-	while (1)
-	{
-		ash = get_input();
-		argv = split(ash);
-		if (argv[0] == NULL)
-			continue;
-		if (compare(argv[0], "exit"))
-		{
-			free(ash);
-			exit(1);
-		}
-		child_process_and_wait(argv);
-		free(ash);
-	}
-       	return (0);
-}
 /**
  * child_process_and_wait - same thing
  * @argv: input
---
+++ main.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+int main(void) {
+	char *ash = malloc(1024), **argv;
+	
+	while (1)
+	{
+		ash = get_input();
+		argv = split(ash);
+		if (argv[0] == NULL)
+			continue;
+		if (compare(argv[0], "exit"))
+		{
+			free(ash);
+			exit(1);
+		}
+		child_process_and_wait(argv);
+		free(ash);
+	}
+       	return (0);
+}
