aa11f7d33602cb637970a3441a7824cf727e834b
soufiane <soufian9mkns@gmail.com>
2023-09-17 12:42:58 -0700

    Freedom 99.99%

---
+++ get_input.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+/**
+ * get_input - reduce lines
+ * Return: char
+ */
+char *get_input()
+{
+	int r;
+	char *ash = (char *)malloc(1024);
+
+	write(STDOUT_FILENO, "($) ", 4);
+	r = read(STDIN_FILENO, ash, 1024);
+	if (r == -1)
+	{
+		write(STDOUT_FILENO, "Error\n", 6);
+		exit(1);
+	}
+	else if (r == 0)
+	{
+		write(STDOUT_FILENO, "\n", 2);
+		exit(0);
+	}
+	return (ash);
+}
