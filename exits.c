c7d7583a77a6e05971352291abc8534ba1463dce
soufiane <soufian9mkns@gmail.com>
2023-09-22 11:26:30 -0700

    existe

---
+++ exits.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int file_exists(const char *file) {
+	char *path = getenv("PATH");
+	if (path == NULL)
+		return 0;
+	char *path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+		perror("strdup");
+        return 0;
+	}
+
+	char *dir = strtok(path_copy, ":");
+	int exists = 0;
+
+	while (dir != NULL)
+	{
+		char *full_path = malloc(strlen(dir) + strlen(file) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			free(path_copy);
+			return 0;
+		}
+		sprintf(full_path, "%s/%s", dir, file);
+		if (access(full_path, F_OK) == 0)
+		{
+			exists = 1;
+		}
+		free(full_path);
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return exists;
+}
